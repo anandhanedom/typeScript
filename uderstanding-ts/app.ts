ee72addcaf075e75a5dee4563ba47d89852d00b4
Anandhan <anandhanedom@gmail.com>
2020-12-18 11:16:15 +0530

    ss

--- uderstanding-ts/app.ts
+++ uderstanding-ts/app.ts
@@ -1,6 +1,37 @@
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+
+// const product = {
+//   id: 'abc1',
+//   price: 12.99,
+//   tags: ['great-offer', 'hot-and-new'],
+//   details: {
+//     title: 'Red Carpet',
+//     description: 'A great carpet - almost brand-new!'
+//   }
+// }
+// This would be the type of such an object:
+
+// {
+//   id: string;
+//   price: number;
+//   tags: string[],
+//   details: {
+//     title: string;
+//     description: string;
+//   }
+// }
+
 const person = {
   name: 'John Doe',
   age: 30,
+  hobbies: ['Sports', 'Cooking'],
 };
 
-console.log(person);
+// let favActivities: any[];
+
+let favActivities: string[];
+
+console.log(person.name);
