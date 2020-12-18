65f4659408654a88bbcefc54670fd16f1a5cd62e
Anandhan <anandhanedom@gmail.com>
2020-12-18 20:34:22 +0530

    s

--- uderstanding-ts/app.js
+++ uderstanding-ts/app.js
@@ -1,21 +1,22 @@
-function combine(n1, n2, resultConversion) {
-    var res;
-    if (typeof n1 === 'number' && typeof n2 === 'number') {
-        res = n1 + n2;
-    }
-    else {
-        res = n1.toString() + n2.toString();
-    }
-    if (resultConversion === 'as-number') {
-        return +res;
-    }
-    else {
-        return res.toString();
-    }
+// function add(n1: number, n2: number): number {
+//   return n1 + n2;
+// }
+function add(n1, n2) {
+    return n1 + n2;
 }
-var combinedAges = combine(30, 26, 'as-number');
-console.log(combinedAges);
-var combinedStringAges = combine('30', '26', 'as-number');
-console.log(combinedStringAges);
-var combinedNames = combine('John', 'Doe', 'as-text');
-console.log(combinedNames);
+function printRes(num) {
+    //:void
+    console.log('Result : ' + num);
+}
+function addAndHandle(n1, n2, cb) {
+    var res = n1 + n2;
+    cb(res);
+}
+printRes(add(5, 12));
+// let combineValues: Function;
+var combineValues;
+combineValues = add;
+// combineValues = printRes; //ERROR
+// combineValues = 5;
+console.log(combineValues(8, 8));
+addAndHandle(10, 20, function (res) { return console.log(res); });
