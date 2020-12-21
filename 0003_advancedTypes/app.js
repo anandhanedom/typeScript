32acc167f21377c6becaaa528c246f5ab9c3bbb9
Anandhan <anandhanedom@gmail.com>
2020-12-21 20:19:21 +0530

    progressing

--- 0003_advancedTypes/app.js
+++ 0003_advancedTypes/app.js
@@ -3,3 +3,54 @@ var e1 = {
     privileges: ['create-server'],
     startDate: new Date()
 };
+function add(a, b) {
+    if (typeof a === 'string' || typeof b === 'string') {
+        //type guard
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+function printEmployeeInfo(emp) {
+    console.log('Name: ', emp.name);
+    if ('privileges' in emp) {
+        console.log('Privileges: ', emp.privileges);
+    }
+    if ('startDate' in emp) {
+        console.log('Start date: ', emp.startDate);
+    }
+}
+printEmployeeInfo(e1);
+var Car = /** @class */ (function () {
+    function Car() {
+    }
+    Car.prototype.drive = function () {
+        console.log('Driving a car...');
+    };
+    return Car;
+}());
+var Truck = /** @class */ (function () {
+    function Truck() {
+    }
+    Truck.prototype.drive = function () {
+        console.log('Driving a truck...');
+    };
+    Truck.prototype.loadCargo = function (amount) {
+        console.log('Loading cargo ... ', amount);
+    };
+    return Truck;
+}());
+var v1 = new Car();
+var v2 = new Truck();
+function useVehicle(vehicle) {
+    vehicle.drive();
+    //   if ('loadCargo' in vehicle) {
+    //     vehicle.loadCargo(1000);
+    //   }
+    //BETTER WAY --
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(1000);
+    }
+}
+useVehicle(v1);
+useVehicle(v2);
+function moveAnimal(animal) { }
--- 0003_advancedTypes/app.ts
+++ 0003_advancedTypes/app.ts
@@ -18,7 +18,81 @@ const e1: ElevatedEmployee = {
   startDate: new Date(),
 };
 
-type Combinable = string | number;
+type Combinable = string | number; //union
 type Numeric = number | boolean;
 
-type Universal = Combinable & Numeric;
+type Universal = Combinable & Numeric; //intersection
+
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === 'string' || typeof b === 'string') {
+    //type guard
+    return a.toString() + b.toString();
+  }
+
+  return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInfo(emp: UnknownEmployee) {
+  console.log('Name: ', emp.name);
+
+  if ('privileges' in emp) {
+    console.log('Privileges: ', emp.privileges);
+  }
+
+  if ('startDate' in emp) {
+    console.log('Start date: ', emp.startDate);
+  }
+}
+
+printEmployeeInfo(e1);
+
+class Car {
+  drive() {
+    console.log('Driving a car...');
+  }
+}
+
+class Truck {
+  drive() {
+    console.log('Driving a truck...');
+  }
+
+  loadCargo(amount: number) {
+    console.log('Loading cargo ... ', amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+
+  //   if ('loadCargo' in vehicle) {
+  //     vehicle.loadCargo(1000);
+  //   }
+
+  //BETTER WAY --
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
+
+interface Bird {
+  flying: number;
+}
+
+interface Horse {
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {}
