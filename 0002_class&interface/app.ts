6d006c10d2a29e99cc1e2ed4e2af43e5b680cd85
Anandhan <anandhanedom@gmail.com>
2020-12-21 15:16:08 +0530

    interface

--- 0002_class&interface/app.ts
+++ 0002_class&interface/app.ts
@@ -1,26 +1,53 @@
-interface Greetable {
-  name: string;
+// type AddFn = (a: number, b: number) => number;
 
+interface AddFn {
+  (a: number, b: number): number;
+}
+
+let add: AddFn;
+
+add = (n1: number, n2: number) => {
+  return n1 + n2;
+};
+
+interface Named {
+  readonly name?: string;
+  outputName?: string;
+
+  greet?(msg: string): void;
+}
+
+interface Greetable extends Named {
   greet(message: string): void;
 }
 
+//you can imlement multiple interfaces
+
 class Person implements Greetable {
-  name: string;
+  name?: string;
 
   age = 30;
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(n?: string) {
+    if (n) {
+      this.name = n;
+    }
   }
 
   greet(message: string) {
-    console.log(message + ' ' + this.name);
+    if (this.name) {
+      console.log(message + ' ' + this.name);
+    } else {
+      console.log('Hi');
+    }
   }
 }
 
 let user1: Greetable;
 
-user1 = new Person('Victor');
+user1 = new Person();
+
+// user1.school = 10; ERROR
 
 user1.greet('Hi there I am');
 console.log(user1);
