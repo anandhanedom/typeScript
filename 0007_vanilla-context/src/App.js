c67fee426dc88aad5339495b862ae1ff24c57559
Anandhan <anandhanedom@gmail.com>
2020-12-30 20:09:50 +0530

    vanilla react usecontext

--- 0007_vanilla-context/src/App.js
+++ 0007_vanilla-context/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Store = React.createContext();
+
+export function Parent(props) {
+  const obj = { text: 'random text' };
+
+  return <Store.Provider value={obj}>{props.children}</Store.Provider>;
 }
 
-export default App;
+export function Child() {
+  const hook = React.useContext(Store);
+
+  return <div>{hook.text}</div>;
+}
