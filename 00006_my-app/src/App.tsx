46489fea6cd3fd66af7789728d2daf39df1f4bf5
Anandhan <anandhanedom@gmail.com>
2020-12-30 16:51:32 +0530

    first project completed

--- 00006_my-app/src/App.tsx
+++ 00006_my-app/src/App.tsx
@@ -1,15 +1,10 @@
-import { type } from 'os';
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 
 type FormElement = React.FormEvent<HTMLFormElement>;
 
 interface ITodo {
   text: string;
   complete: boolean;
-}
-
-interface ITodo2 extends ITodo {
-  tags: string[];
 }
 
 function App() {
@@ -19,13 +14,32 @@ function App() {
   const handleSubmit = (e: FormElement): void => {
     e.preventDefault(); //prevent from refreshing
 
+    addTodo(value);
+
     setValue('');
   };
 
   // debugger;
 
+  const addTodo = (text: string): void => {
+    const newTodos: ITodo[] = [...todos, { text, complete: true }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = (index: number): void => {
+    const newTodos: ITodo[] = [...todos];
+    newTodos[index].complete = !newTodos[index].complete;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (index: number): void => {
+    const newTodos: ITodo[] = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
   return (
-    <div className="App">
+    <div className="App" style={{ padding: '20px' }}>
       <h1>Todo List</h1>
       <form onSubmit={handleSubmit}>
         <input
@@ -36,6 +50,23 @@ function App() {
         />
         <button type="submit">Add Todo</button>
       </form>
+      <section>
+        {todos.map((todo: ITodo, i: number) => (
+          <Fragment key={i}>
+            <div
+              style={{ textDecoration: todo.complete ? '' : 'line-through' }}
+            >
+              {todo.text}
+            </div>
+            <button type="button" onClick={() => completeTodo(i)}>
+              {todo.complete ? 'Complete' : 'Incomplete'}
+            </button>
+            <button type="button" onClick={() => removeTodo(i)}>
+              Remove Todo
+            </button>
+          </Fragment>
+        ))}
+      </section>
     </div>
   );
 }
