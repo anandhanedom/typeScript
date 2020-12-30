aa867b582ce88fce0f743b153404ab8ba1d1a1fb
Anandhan <anandhanedom@gmail.com>
2020-12-30 14:45:55 +0530

    React TS

--- 00006_my-app/src/App.tsx
+++ 00006_my-app/src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { type } from 'os';
+import React, { useState } from 'react';
+
+type FormElement = React.FormEvent<HTMLFormElement>;
+
+interface ITodo {
+  text: string;
+  complete: boolean;
+}
+
+interface ITodo2 extends ITodo {
+  tags: string[];
+}
 
 function App() {
+  const [value, setValue] = useState<string>('');
+  const [todos, setTodos] = useState<ITodo[]>([]);
+
+  const handleSubmit = (e: FormElement): void => {
+    e.preventDefault(); //prevent from refreshing
+
+    setValue('');
+  };
+
+  // debugger;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          required
+        />
+        <button type="submit">Add Todo</button>
+      </form>
     </div>
   );
 }
