36289609b9a2311abfca1258b5c8d051e00150f4
Anandhan <anandhanedom@gmail.com>
2020-12-31 11:08:40 +0530

    s

--- 0008_episode-picker/src/App.tsx
+++ 0008_episode-picker/src/App.tsx
@@ -1,12 +1,29 @@
-import React, { Fragment, useContext } from 'react';
+import React, { Fragment, useContext, useEffect } from 'react';
 import { Store } from './Store';
 
 function App(): JSX.Element {
-  const store = useContext(Store);
+  const { state, dispatch } = useContext(Store);
+
+  useEffect(() => {
+    state.episodes.length === 0 && fetchDataAction();
+  });
+
+  const fetchDataAction = async () => {
+    const url =
+      'https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes';
+    const data = await fetch(url);
+    const dataJson = await data.json();
+
+    return dispatch({
+      type: 'FETCH_DATA',
+      payload: dataJson._embedded.episodes,
+    });
+  };
+
+  console.log(state);
 
   return (
     <Fragment>
-      {console.log(store)}
       <h1>Rick and Morty</h1>
       <p>Pick your favorite episode!</p>
     </Fragment>
--- 0008_episode-picker/src/Store.tsx
+++ 0008_episode-picker/src/Store.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
 interface IState {
   episodes: [];
   favorites: [];
+}
+
+interface IAction {
+  type: string;
+  payload: any;
 }
 
 const INITIAL_STATE: IState = {
@@ -10,14 +15,24 @@ const INITIAL_STATE: IState = {
   favorites: [],
 };
 
-export const Store = React.createContext<IState>(INITIAL_STATE);
+export const Store = React.createContext<IState | any>(INITIAL_STATE);
 
-function reducer() {
-  //pass
+function reducer(state: IState, action: IAction): IState {
+  switch (action.type) {
+    case 'FETCH_DATA':
+      return { ...state, episodes: action.payload };
+
+    default:
+      return state;
+  }
 }
 
 export function StoreProvider(props: any): JSX.Element {
+  const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
+
   return (
-    <Store.Provider value={INITIAL_STATE}>{props.children}</Store.Provider>
+    <Store.Provider value={{ state, dispatch }}>
+      {props.children}
+    </Store.Provider>
   );
 }
